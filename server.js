814b0d9e89cd12c44c5302701601d7672156b44d
JordanWinslow <admin@jordanwinslow.me>
2019-08-28 18:00:10 -0700

    added 404 page

--- server.js
+++ server.js
@@ -12,5 +12,11 @@ const server = express()
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(adminRoutes)
 server.use(userRoutes)
-
+server.use((request, response, next) => {
+  response
+    .status(404)
+    .send(
+      "<title>404 - Page not Found</title><h1>404 - Page Not Found</h1><p><a href='/'>Click Here</a> to return to the home page.</p>"
+    )
+})
 server.listen(PORT)
